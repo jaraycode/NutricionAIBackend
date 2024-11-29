f88680f43fb2fab5abad1cbeea758745385e188a
jaraycode <jonasaray12@gmail.com>
2024-11-29 07:36:44 -0400

    Fix: changes on login of the routers and name of function of the JWT

--- src/app/Utils/auth.py
+++ src/app/Utils/auth.py
@@ -12,7 +12,7 @@ def encryptPassword(password: str) -> str:
 def validatePassword(password:str, encrypted: str) -> bool:
     return bcrypt.checkpw(password.encode("utf-8"), encrypted.encode("utf-8"))
 
-def signJWT(username: str) -> Dict[str,str]:
+def generateJWToken(username: str) -> Dict[str,str]:
     EXPIRES = datetime.now(tz=timezone.utc) - timedelta(days=30)
 
     payload = {
